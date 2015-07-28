5790504100efd6379376d46699aa1528b613e9ee
ujjwal singh <>
2015-07-28 12:57:39 +0530

    changed naming

--- src/main/java/com/vertex/HttpVerticle.java
+++ src/main/java/com/vertex/HttpVerticle.java
@@ -1,6 +1,7 @@
-package vertex;
+package com.vertex;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
@@ -9,26 +10,34 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 
 public class HttpVerticle extends AbstractVerticle {
-	
-	@Override
-	public void start(Future<Void> startFuture){
+
+	public void start(Future<Void> startFuture) {
 		HttpServer server = vertx.createHttpServer();
 		server.requestHandler(new Handler<HttpServerRequest>() {
-			
+
 			public void handle(HttpServerRequest req) {
 				// TODO Auto-generated method stub
 				req.response().end("Hello World!");
-				
+
 			}
 		});
+
+		server.listen(8080, "localhost",
+				new Handler<AsyncResult<HttpServer>>() {
+					public void handle(AsyncResult<HttpServer> res) {
+						//System.out.println(res.result().toString());
+					}
+				});
+
 		System.out.println("MyVerticle Started!");
+		startFuture.complete();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VertxOptions options = new VertxOptions().setWorkerPoolSize(10);
 		Vertx vertx = Vertx.vertx(options);
-		vertx.deployVerticle("com.mydomain.HttpVerticle");
+		vertx.deployVerticle("com.vertex.HttpVerticle");
 
 	}
 
