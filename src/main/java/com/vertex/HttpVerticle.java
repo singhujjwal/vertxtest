efa555855a596fdd4a87c258b81f973b0a582b22
ujjwal singh <>
2015-08-05 14:10:11 +0530

    .

--- src/main/java/com/vertex/HttpVerticle.java
+++ src/main/java/com/vertex/HttpVerticle.java
@@ -6,6 +6,9 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 
@@ -17,8 +20,16 @@ public class HttpVerticle extends AbstractVerticle {
 
 			public void handle(HttpServerRequest req) {
 				// TODO Auto-generated method stub
-				req.response().end("Hello World!");
-
+				//req.response().end("Hello World!");
+				
+				req.bodyHandler(new Handler<Buffer>() {
+					public void handle(Buffer buff){
+						EventBus eb = vertx.eventBus();
+						//This vertex published whatever it gets from the request buffer
+						//eb.publish("Channel1", "Hello can i board the bus");
+						eb.publish("Channel1", buff.toString());
+					}
+				});
 			}
 		});
 
@@ -37,8 +48,15 @@ public class HttpVerticle extends AbstractVerticle {
 		// TODO Auto-generated method stub
 		VertxOptions options = new VertxOptions().setWorkerPoolSize(10);
 		Vertx vertx = Vertx.vertx(options);
-		vertx.deployVerticle("com.vertex.HttpVerticle");
-
+		
+		
+		vertx.eventBus().publish("Channel1", "message2");
+		
+		
+//		vertx.deployVerticle("com.vertex.HttpVerticle");
+//		vertx.deployVerticle("com.vertex.HttpVerticle2");
+		vertx.deployVerticle("com.mydomain.RouterVerticle");
+		
 	}
 
 }
